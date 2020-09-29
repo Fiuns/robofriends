5d06977d5f56ca39447b8fc156abb5654ad8f027
Fiuns <andres.funes.sa@gmail.com>
2020-09-29 17:58:59 -0500

    adding updated files

---
+++ src/components/SearchBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const SearchBox = ({searchField, searchChange}) => {
+
+    return (
+        <React.Fragment >
+        <div className='pa2'>
+            <input 
+                className = 'pa3 ba b--green bg-lightest-blue'
+                type='search'
+                placeholder='search robots'
+                onChange={searchChange}
+
+            />
+        </div>
+        
+        </React.Fragment>
+        
+    );
+}
+
+export default SearchBox;
